a90d75f705ed4eac813717d3350e50af9bf58c08
ylaguardia <ylaguardia@gmail.com>
2021-07-27 19:05:32 -0300

    Fix CSS bugs

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,22 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+import { StylesProvider, createGenerateClassName } from '@material-ui/styles';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ctner',
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
 
-        <MarketingApp />
-      </div>
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
